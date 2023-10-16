5d0e2ffb0952e0730a03beed45da7f2ae56de737
Clement <baafik05@gmail.com>
2023-10-16 14:52:55 +0000

    empty auth

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:games_services/games_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
